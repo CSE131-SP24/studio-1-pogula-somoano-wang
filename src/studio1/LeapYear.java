2b33715e36d547b8237364b8349f64d6a956b98d
sahithipogula <sahithipogula@localhost>
2024-01-24 15:26:06 -0600

    completed!

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,21 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Year: ");
+		int year  = in.nextInt();
+		
+		boolean check;
+		
+		check = ((year%4 == 0) && !(year%100 == 0)) || (year%400 == 0);
+		
+		System.out.println(year + " is a leap year: " + check);
+		
+
+	}
+
+}
